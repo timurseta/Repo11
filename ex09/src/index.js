76d580dae050bfee03916ba5e0d14aad9f845a80
timurseta <timur_seta@hotmail.com>
2021-09-24 09:18:09 +0200

    falsy values

--- ex09/src/index.js
+++ ex09/src/index.js
@@ -1,6 +1,8 @@
 // Only change code below this line
-
+function myBouncer(arr) {
+    return arr = arr.filter(Boolean);
+}
 // Only change code above this line
 
-console.log(myBouncer());
+console.log(myBouncer([false, null, 0, NaN, undefined, ""]));
 module.exports = myBouncer;
