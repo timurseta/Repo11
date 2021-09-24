c4ea766b4dfb5f482bcb0af2c42ad27af234a500
timurseta <timur_seta@hotmail.com>
2021-09-24 10:00:11 +0200

    used splice

--- ex10/src/index.js
+++ ex10/src/index.js
@@ -1,6 +1,9 @@
 // Only change code below this line
-
+function mySplice(arr1, arr2, n){
+    arr2.splice(n, 0, ...arr1.reverse());
+    return arr2;
+}
 // Only change code above this line
 
-console.log(mySplice());
+console.log(mySplice(["Dell", "Phillips"], ["Samsung", "AOX", "Sony"], 2));
 module.exports = mySplice;
